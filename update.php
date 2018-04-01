c85d4e75121c0e1a7487581c040d873a359fc4aa
eimymendezh <33436997+eimymendezh@users.noreply.github.com>
2018-04-01 00:33:40 -0500

    1

--- update.php
+++ update.php
@@ -31,7 +31,7 @@ echo "<ul>
 }
 if (isset($_POST['new']))
 {
-$result1 = pg_query($db, "UPDATE users SET id = ('".$_POST["id_update"]."', '".$_POST["username_update"]."', '".$_POST["password_updated"]."')";
+$result1 = pg_query($db, "UPDATE users SET id = '$_POST[id_update]', username = '$_POST[username_update]', password = '$_POST[password_updated]'");
 if (!$result1)
 {
 echo "Update failed!!";
