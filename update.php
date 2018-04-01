60965cde9ecb4895d055fc0a2167b5a2bacd7892
eimymendezh <33436997+eimymendezh@users.noreply.github.com>
2018-04-01 16:26:05 -0500

    c

--- update.php
+++ update.php
@@ -15,3 +15,29 @@ li {list-style: none;}
 </ul>
 </body>
 </html>
+<?php
+$db = pg_connect("host=localhost port=5432 dbname=eimybase user=postgres password=teleco");
+$result = pg_query($db, "SELECT * FROM users where id = '$_POST[id]'");
+$row = pg_fetch_assoc($result);
+if (isset($_POST['submit']))
+{
+echo "<ul>
+<form name='update' action='update.php' method='POST' >
+<li>ID:</li><li><input type='text' name='id_updated' value='$row[id]'  /></li>
+<li>Name:</li><li><input type='text' name='name_updated' value='$row[username]' /></li>
+<li>Password:</li><li><input type='text' name='pass_updated' value='$row[password]' /></li>
+<li><input type='submit' name='new' /></li>  </form>
+</ul>";
+}
+if (isset($_POST['new']))
+{
+$result1 = pg_query($db, "UPDATE users SET id = '$_POST[id_updated]', username = '$_POST[name_updated]', password = '$_POST[pass_updated]' WHERE id = '$_POST[id]'");
+if (!$result1)
+{
+echo "Update failed!!";
+} else
+{
+echo "Update successfull;";
+}
+}
+?>
