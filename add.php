3cc074fb3d419c95dc4c593bd922f8bb5820067e
eimymendezh <33436997+eimymendezh@users.noreply.github.com>
2018-03-31 23:41:07 -0500

    Initial commit

---
+++ add.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<head>
+<title>Usuarios</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<style>
+li {listt-style: none;}
+</style>
+</head>
+<body>
+<h2>Enter information</h2>
+<ul>
+<form name="insert" action="insert.php" method="POST" >
+<li>ID:</li><li><input type="text" name="id" /></li>
+<li>Name:</li><li><input type="text" name="username" /></li>
+<li>Password:</li><li><input type="text" name="password" /></li>
+<li><input type="submit" /></li>
+</form>
+</ul>
+</body>
+</html>
+<?php
+$db = pg_connect("host=localhost port=5432 dbname=eimybase user=postgres password=teleco");
+$query = "INSERT INTO users VALUES ('$_POST[id]','$_POST[username]','$_POST[password])";
+$result = pg_query($query); 
+?>
